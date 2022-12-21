38848a1baf41ca6b07d205629c30edd6d2f82af5
streiff1337 <sas.oss@mail.ru>
2022-12-21 21:59:27 +0700

    InteractiveRaycast

--- Assets/LearnMaterials 2/Scripts/InteractiveRaycast.cs
+++ Assets/LearnMaterials 2/Scripts/InteractiveRaycast.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class InteractiveRaycast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject obj;
+    public GameObject obj2;
+    InteractiveBox thisBox;
+
+    private IEnumerator onClickCustom()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit, 100f))
+            {
+                if (raycastHit.transform != null)
+                {
+                    if (raycastHit.transform.CompareTag("InteractivePlane"))
+                    {
+                        Instantiate(obj, raycastHit.point + raycastHit.normal / 2, new Quaternion());
+                    }
+                    if (raycastHit.transform.TryGetComponent<InteractiveBox>(out InteractiveBox boxItem))
+                    {
+                        if (thisBox == null)
+                        {
+                            thisBox = boxItem;
+                        }
+                        else
+                        {
+                            thisBox.gameObject.GetComponent<InteractiveBox>().AddNext(boxItem);
+                            thisBox = null;
+                        }
+                    }
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit, 100f))
+            {
+                if (raycastHit.transform != null)
+                {
+                    if (raycastHit.transform.CompareTag("InteractivePlane"))
+                    {
+                        Instantiate(obj2, raycastHit.point + raycastHit.normal / 2, new Quaternion());
+                    }
+                    if (raycastHit.transform.TryGetComponent<InteractiveBox>(out InteractiveBox boxItem))
+                    {
+                        if (thisBox == null)
+                        {
+                            thisBox = boxItem;
+                        }
+                        else
+                        {
+                            thisBox.gameObject.GetComponent<InteractiveBox>().AddNext(boxItem);
+                            thisBox = null;
+                        }
+                    }
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit, 100f))
+            {
+                if (raycastHit.transform != null)
+                {
+                    if (raycastHit.transform.TryGetComponent<InteractiveBox>(out InteractiveBox boxItem))
+                    {
+                        Destroy(raycastHit.transform.gameObject);
+                    }
+                }
+            }
+        }
+        yield return null;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        
+        StartCoroutine(onClickCustom());
+
     }
 }
+
