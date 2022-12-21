f68cb0fef657a25d71d9e494e06266a35b8d4947
Alexander <79638754+Satavo@users.noreply.github.com>
2022-12-21 21:55:51 +0700

    InteractiveBox

--- Assets/LearnMaterials 2/Scripts/InteractiveBox.cs
+++ Assets/LearnMaterials 2/Scripts/InteractiveBox.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class InteractiveBox : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask obstacleLayerMask;
+    [SerializeField]
+    InteractiveBox next;
+    [SerializeField]
+    [ContextMenu("Запуск ракет")]
+
+    public void AddNext(InteractiveBox box)
+    {
+        next = box;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    void FixedUpdate()
+    {
+        if (next != null)
+        {
+            if (Physics.Linecast(transform.position, next.transform.position, out RaycastHit hitbox, obstacleLayerMask))
+            {
+                Debug.Log("Kill");
+                if (hitbox.transform.TryGetComponent<ObstacleItem>(out ObstacleItem obstacleItem))
+                {
+                    obstacleItem.GetDamage();
+                }
+            }
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        if (next != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, next.transform.position);
+        }
+
     }
 }
